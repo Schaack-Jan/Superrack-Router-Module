bb9ed1932f4857ff11fb77f7e9206221efe0876a
Jan Schaack <janschaack@gmail.com>
2025-12-30 04:09:07 +0100

    feat: add default hot plugin and snapshot mappings to MIDI settings

--- default-variables.js
+++ default-variables.js
@@ -11,13 +11,43 @@ const getEmptyTemplate = (rackCount) => {
 	return racks
 }
 
+const getHotPluginMapping = () => {
+	return [
+		null,
+		{ id: 1, value: 1 },
+		{ id: 2, value: 2 },
+		{ id: 3, value: 3 },
+		{ id: 4, value: 4 },
+		{ id: 5, value: 5 },
+		{ id: 6, value: 6 },
+		{ id: 7, value: 7 },
+		{ id: 8, value: 8 },
+		{ id: 9, value: 9 },
+		{ id: 10, value: 10 },
+		{ id: 11, value: 11 },
+		{ id: 12, value: 12 },
+	]
+}
+
+const getHotSnapshotMapping = () => {
+	return [
+		null,
+		{ id: 1, value: 1 },
+		{ id: 2, value: 2 },
+		{ id: 3, value: 3 },
+		{ id: 4, value: 4 },
+		{ id: 5, value: 5 },
+		{ id: 6, value: 6 },
+	]
+}
+
 module.exports = {
 	rackCount: 64,
 	channelCount: 128,
 	midi: superrackMidiMap,
 	httpSettings: { server: null, port: 8010, started: false },
 	logLevel: 'error',
-	hotPlugin: { type: 'cc', channel: 1, mapping: getEmptyTemplate(12), emptyMapping: getEmptyTemplate(12) },
-	hotSnapshot: { type: 'cc', channel: 2, mapping: getEmptyTemplate(6), emptyMapping: getEmptyTemplate(6) },
+	hotPlugin: { type: 'cc', channel: 1, mapping: getHotPluginMapping(), emptyMapping: getEmptyTemplate(12) },
+	hotSnapshot: { type: 'cc', channel: 2, mapping: getHotSnapshotMapping(), emptyMapping: getEmptyTemplate(6) },
 	mapping: getEmptyTemplate,
 }
