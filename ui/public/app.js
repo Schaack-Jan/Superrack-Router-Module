ff51b8d39295a7a69c2f3da18841228c02b5e833
Micha Gehringer <michagehringer@t-online.de>
2026-02-12 11:45:33 +0100

    fix: remove debug logging from persistMappingUpdate function

--- ui/public/app.js
+++ ui/public/app.js
@@ -269,7 +269,6 @@ const persistMappingUpdate = async (changeYId, previousValue) => {
     try {
         const res = await sendMatrixToServer()
         const data = await res.json()
-        console.log('[MAPPING_UI] Persist response', data)
         if (!(data && data.success !== false)) {
             throw new Error('Backend reports an error during patch update')
         }
